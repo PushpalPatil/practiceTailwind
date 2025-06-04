49bcf6ffa12781058abf9abb168adacce4069efe
Pushpal Patil <gauripushp@gmail.com>
2025-06-03 20:07:35 -0400

    the button finally works

--- practice/src/Task.tsx
+++ practice/src/Task.tsx
@@ -1,32 +1,37 @@
+export type TaskData = {
+  id: number,
+  name: string,
+  description: string,
+  selected: boolean
+}
 
-import './App.css';
+type TaskProps = {
+  taskData: TaskData,
+  toggleSelected: () => void
+}
 
-const initializeTasks: TaskData[] = [
-  {
-    id: 0,
-    name: "Sweep the Kitchen",
-    description: "Get under the cabinets, do a good job",
-    completed ?: false
-  }
-]
+function Task({taskData, toggleSelected} : TaskProps){
 
+    const selected = taskData.selected
+    const divBase = "flex flex-row items-center w-xl m-5 border-1 border-gray-300 pl-3.5 rounded-xl "
+    const divFinal = divBase + "bg-green-100 border-green-100"
 
-function list(){
-  const [tasks, setTasks] = useState(initializeTasks)
+    const hover = 'hover:bg-green-600'
+    const buttonTransition = 'h-2 p-2.5 rounded-md border-1 border-gray-400 '
+    const buttonBase = buttonTransition + hover
+    const buttonFinal = buttonTransition + "bg-green-600"
 
-  const taskCompleted = (id: number): void =>{
-    setTasks((p) =>{
-      const newTask = structuredClone(p)
-      const changeTask = newTask.find((task) => task.id === id)
-      if(!changeTask) return newTask
-      changeTask.completed = !changeTask.completed
-      return newTask
-    })
-  }
+    return(
+        <div className={selected? divFinal : divBase}>
+            <button onClick={toggleSelected} className={selected? buttonFinal : buttonBase}></button>
+            <div className="p-3.5 flex flex-col">
+                <div className="font-medium  font-[inter] text-md pb-0.5"> {taskData.name}</div>
+                <div className="font- light font-[inter] text-sm text-gray-400"> {taskData.description}</div>
+            </div>
 
-  return{
-    
-  }
+        </div>
+    )
+
 }
 
 export default Task
